3da2c27ba22fdeb1aeefb861bacb266502e7d38b
javier14-dev <jcondorich@unjbg.edu.pe>
2025-06-11 20:27:49 -0500

    actualiza ejemplo3: mejora del ejercicio3

--- ejemplo3.cpp
+++ ejemplo3.cpp
@@ -1,17 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main(){
-    int n,aux,A[20];
+    int aux,n,A[20];
     cin>>n;
-    s=0;
-    for(i=0;i<n;i++){
-        cin>>A[i];
+    for (int i=1; i<=n; i++) {
+		cin>>A[i];
     }
-    for(i=1;;i++){
-        aux=A[i]
+    for (int i=1; i<=(n/2); i++) {
+        aux=A[i];
+        A[i]=A[n-i+1];
+        A[n-i+1]=aux;
     }
-
-
-
+    cout<<" "<<endl;
+    for (int i=1; i<=n;i++) {
+        cout<<A[i]<<";";
+    }
     return 0;
 }
