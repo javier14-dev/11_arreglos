9f229d89d0246e98224c8bc1aa95d1cc43563c9c
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-07-09 20:33:11 -0500

    Mejorar nombres de las variables

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    int numeroElementos, numeroMayor, A[20];
+    int numeroElementos, numeroMayor, Arreglo[20];
     cout << "Ingrese el cantidad de elementos del arreglo: ";
     cin >> numeroElementos;
     for (int i = 0; i < numeroElementos; i++) {
         cout << "Ingrese el elemento " << i + 1 << " del arreglo: ";
-        cin >> A[i];
+        cin >> Arreglo[i];
     }
-    numeroMayor = A[0];
-    for (int i = 1; i < n; i++) {
-        if (A[i] > numeroMayor) {
-            numeroMayor = A[i];
+    numeroMayor = Arreglo[0];
+    for (int i = 1; i < numeroElementos; i++) {
+        if (Arreglo[i] > numeroMayor) {
+            numeroMayor = Arreglo[i];
         }
     }
     cout << "El mayor elemento del arreglo es: "<< numeroMayor;
