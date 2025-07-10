848e779d213b738d664c2daf19a7344bb031acdc
JosueMaquera <jmaquerap@unjbg.edu.pe>
2025-07-09 20:56:47 -0500

    Agregar la funcionalidad de sumar elementos del arreglo

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 int numeroMayor(int arreglo[], int numeroElementos);
+
 int main() {
-    int numeroElementos, mayor, Arreglo[100], i;
+    int numeroElementos, mayor, Arreglo[100], i, sumaElementos;
+    sumaElementos = 0;
     cout << "Ingrese el cantidad de elementos del arreglo: ";
     cin >> numeroElementos;
     for (i = 0; i < numeroElementos; i++) {
         cout << "Ingrese el elemento " << i + 1 << " del arreglo: ";
         cin >> Arreglo[i];
+        sumaElementos += Arreglo[i];
     }
 
     mayor = numeroMayor(Arreglo, numeroElementos);
-    cout << "El mayor elemento del arreglo es: "<< mayor;
+    cout << "La suma de elementos del arreglo es: " << sumaElementos << endl;
+    cout << "El mayor elemento del arreglo es: "<< mayor << endl;
     return 0;
 }
 
